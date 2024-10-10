ea4c013f828a04aa4fdb39acd627233b6de52555
Yuni-Yuni <105819105+Yuni-Yuni@users.noreply.github.com>
2024-10-10 03:05:39 +0300

    Initial commit

---
+++ app/src/main/java/com/example/festiva/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.festiva;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.festiva.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        //setContentView(R.layout.activity_main);
+        setContentView(binding.getRoot());
+
+        loadFragment(new HomeFragment());
+
+
+        binding.bottomNav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (id == R.id.more) {
+                    loadFragment(new MoreFragment());
+                } else if (id == R.id.profile) {
+                    loadFragment(new ProfileFragment());
+                }
+
+                return true;
+            }
+        });
+    }
+
+    private void loadFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.fragment_container , fragment);
+        transaction.commit();
+    }
+}
